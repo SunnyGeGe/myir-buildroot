6051b8e23ed2ad30bc9494b6f565145301f5de9c
sunny.guo <sunny.guo@myirtech.com>
2016-12-22 14:24:10 +0800

    MYIRTECH: Build ramdisk for myd_c437x_pru board

--- board/myir/myd_c437x_idk/post-image.sh
+++ board/myir/myd_c437x_idk/post-image.sh
@@ -6,6 +6,11 @@ BOARD_DIR="$(dirname $0)"
 
 # copy the uEnv.txt to the output/images directory
 cp board/myir/myd_c437x_idk/uEnv.txt $BINARIES_DIR/uEnv.txt
+cp board/myir/myd_c437x_idk/uEnv_mmc.txt $BINARIES_DIR/uEnv_mmc.txt
+cp board/myir/myd_c437x_idk/uEnv_ramdisk.txt $BINARIES_DIR/uEnv_ramdisk.txt
+cp board/myir/myd_c437x_idk/readme.txt $BINARIES_DIR/readme.txt
+mkimage -A arm -O linux -T ramdisk -C none -a 0x88080000 -n "ramdisk" -d $BINARIES_DIR/rootfs.cpio.gz $BINARIES_DIR/ramdisk.gz
+
 
 GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
