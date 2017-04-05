7beb2b029c6520505c94147fda575acf73ee001a
sunny.guo <sunny.guo@myirtech.com>
2017-04-05 09:10:25 +0800

    MYIRTECH: Fix an error during re-building myd am335x series
    Fix the error:
    ln: failed to create symbolic link '/home/user/src/myir-buildroot/output/target/lib/firmware/am335x-pru0-fw': File exists
    Makefile:651: recipe for target 'target-finalize' failed
    make[1]: *** [target-finalize] Error 1
    Makefile:79: recipe for target '_all' failed
    make: *** [_all] Error 2

--- board/myir/myd_y335x/post-build.sh
+++ board/myir/myd_y335x/post-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
 if [ -e ${TARGET_DIR}/lib/firmware/pru/PRU_Halt.out ]; then
-	ln -s /lib/firmware/pru/PRU_Halt.out ${TARGET_DIR}/lib/firmware/am335x-pru0-fw
-	ln -s /lib/firmware/pru/PRU_Halt.out ${TARGET_DIR}/lib/firmware/am335x-pru1-fw
+	ln -sf /lib/firmware/pru/PRU_Halt.out ${TARGET_DIR}/lib/firmware/am335x-pru0-fw
+	ln -sf /lib/firmware/pru/PRU_Halt.out ${TARGET_DIR}/lib/firmware/am335x-pru1-fw
 fi
