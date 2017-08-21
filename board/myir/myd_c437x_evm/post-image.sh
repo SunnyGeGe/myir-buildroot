303b8bc58321d76d1e5904a8c7caa193276bac87
qinlonghua <long.qin@myirtech.com>
2017-08-21 19:44:10 +0800

    add create matrix rootfs

--- board/myir/myd_c437x_evm/post-image.sh
+++ board/myir/myd_c437x_evm/post-image.sh
@@ -17,6 +17,28 @@ cp board/myir/myd_c437x_evm/uEnv_ramdisk.txt $BINARIES_DIR/uEnv/uEnv_ramdisk.txt
 cp board/myir/myd_c437x_evm/readme.txt $BINARIES_DIR/readme.txt
 mkimage -A arm -O linux -T ramdisk -C none -a 0x88080000 -n "ramdisk" -d $BINARIES_DIR/rootfs.cpio.gz $BINARIES_DIR/ramdisk.gz
 
+cp board/myir/myd_c437x_evm/tisdk-rootfs-image-am437x-evm.tar.gz $BINARIES_DIR/matrix-rootfs.tar.gz
+
+mkdir ./tmp
+tar xvf $BINARIES_DIR/matrix-rootfs.tar.gz -C ./tmp
+tar xvf $BINARIES_DIR/rootfs.tar ./lib/modules/4.1.18
+
+cp -a ./tmp/lib/modules/4.1.18-gbbe8cfc/extra/* ./lib/modules/4.1.18/extra/
+cp $BINARIES_DIR/zImage ./tmp/boot
+cp $BINARIES_DIR/myd_c437x_evm.dtb ./tmp/boot
+
+rm ./lib/modules/4.1.18/extra/omapdrm_pvr.ko
+sed -i '/omapdrm_pvr/d' ./lib/modules/4.1.18/modules.dep
+sed -i '$a\extra/pvrsrvkm.ko:' ./lib/modules/4.1.18/modules.dep
+sed -i '$a\extra/bc_example.ko: extra/pvrsrvkm.ko' ./lib/modules/4.1.18/modules.dep
+sed -i '$a\extra/cryptodev.ko:' ./lib/modules/4.1.18/modules.dep
+
+cp -a ./lib/modules/4.1.18/ ./tmp/lib/modules/
+gzip -d $BINARIES_DIR/matrix-rootfs.tar.gz
+tar rvf $BINARIES_DIR/matrix-rootfs.tar ./tmp/lib/modules/4.1.18/
+tar rvf $BINARIES_DIR/matrix-rootfs.tar ./tmp/boot/
+gzip -9 -c $BINARIES_DIR/matrix-rootfs.tar  > $BINARIES_DIR/matrix-rootfs.tar.gz
+rm -rf ./lib/modules/4.1.18
 
 GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
