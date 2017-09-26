2084c7eeeed1658c022d6b62fd599bf6f3b39d86
sunny.guo <sunny.guo@myirtech.com>
2017-09-26 14:29:11 +0800

    MYIRTECH: change updatesys to support emmc and nand

--- board/myir/myd_y335x/post-image.sh
+++ board/myir/myd_y335x/post-image.sh
@@ -6,7 +6,21 @@ BOARD_DIR="$(dirname $0)"
 
 # copy the uEnv.txt to the output/images directory
 cp board/myir/myd_y335x/uEnv.txt $BINARIES_DIR/uEnv.txt
-cp board/myir/myd_y335x/uEnv_ramdisk.txt $BINARIES_DIR/uEnv_ramdisk.txt
+cp board/myir/myd_y335x/uEnv_sd.txt $BINARIES_DIR/uEnv_sd.txt
+cp board/myir/myd_y335x/uEnv_mmc.txt $BINARIES_DIR/uEnv_mmc.txt
+cp board/myir/myd_y335x/uEnv_usbmsc.txt $BINARIES_DIR/uEnv_usbmsc.txt
+cp board/myir/myd_y335x/uEnv_sd_ramdisk.txt $BINARIES_DIR/uEnv_sd_ramdisk.txt
+cp board/myir/myd_y335x/uEnv_usbmsc_ramdisk.txt $BINARIES_DIR/uEnv_usbmsc_ramdisk.txt
+cp board/myir/myd_y335x/MLO $BINARIES_DIR/MLO
+cp board/myir/myd_y335x/MLO_emmc $BINARIES_DIR/MLO_emmc
+cp board/myir/myd_y335x/MLO_nand $BINARIES_DIR/MLO_nand
+cp board/myir/myd_y335x/MLO_sd $BINARIES_DIR/MLO_sd
+cp board/myir/myd_y335x/MLO_usbmsc $BINARIES_DIR/MLO_usbmsc
+cp board/myir/myd_y335x/u-boot.img $BINARIES_DIR/u-boot.img
+cp board/myir/myd_y335x/u-boot_sd.img $BINARIES_DIR/u-boot_sd.img
+cp board/myir/myd_y335x/u-boot_emmc.img $BINARIES_DIR/u-boot_emmc.img
+cp board/myir/myd_y335x/u-boot_nand.img $BINARIES_DIR/u-boot_nand.img
+cp board/myir/myd_y335x/u-boot_usbmsc.img $BINARIES_DIR/u-boot_usbmsc.img
 cp board/myir/myd_y335x/readme.txt $BINARIES_DIR/readme.txt
 mkimage -A arm -O linux -T ramdisk -C none -a 0x88080000 -n "ramdisk" -d $BINARIES_DIR/rootfs.cpio.gz $BINARIES_DIR/ramdisk.gz
 
