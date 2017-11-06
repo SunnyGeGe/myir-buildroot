b6df6f09ea31b7623e7834e8def6cc7f2fbafe67
qinlonghua <long.qin@myirtech.com>
2017-11-06 16:21:06 +0800

    MYIRTECH: delete extra dir

--- board/myir/myir_ricoboard/post-image.sh
+++ board/myir/myir_ricoboard/post-image.sh
@@ -17,20 +17,7 @@ cp board/myir/myir_ricoboard/uEnv_ramdisk.txt $BINARIES_DIR/uEnv/uEnv_ramdisk.tx
 cp board/myir/myir_ricoboard/readme.txt $BINARIES_DIR/readme.txt
 mkimage -A arm -O linux -T ramdisk -C none -a 0x88080000 -n "ramdisk" -d $BINARIES_DIR/rootfs.cpio.gz $BINARIES_DIR/ramdisk.gz
 
-cp board/myir/myir_ricoboard/tisdk-rootfs-image-am437x-evm.tar.gz $BINARIES_DIR/matrix-rootfs.tar.gz
 
-tar xvf $BINARIES_DIR/matrix-rootfs.tar.gz ./lib/modules/4.1.18-gbbe8cfc
-tar xvf $BINARIES_DIR/rootfs.tar ./lib/modules/4.1.18
-cp  ./lib/modules/4.1.18-gbbe8cfc/extra ./lib/modules/4.1.18 -rf
-rm ./lib/modules/4.1.18/extra/omapdrm_pvr.ko
-sed -i '/omapdrm_pvr/d' ./lib/modules/4.1.18/modules.dep
-sed -i '$a\extra/pvrsrvkm.ko:' ./lib/modules/4.1.18/modules.dep
-sed -i '$a\extra/bc_example.ko: extra/pvrsrvkm.ko' ./lib/modules/4.1.18/modules.dep
-sed -i '$a\extra/cryptodev.ko:' ./lib/modules/4.1.18/modules.dep
-gzip -d $BINARIES_DIR/matrix-rootfs.tar.gz
-tar rvf $BINARIES_DIR/matrix-rootfs.tar ./lib/modules/4.1.18
-gzip -9 -c $BINARIES_DIR/matrix-rootfs.tar  > $BINARIES_DIR//matrix-rootfs.tar.gz
-rm -rf ./lib
 
 GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
