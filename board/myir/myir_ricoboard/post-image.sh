f16cd24fa64adaacd44fd02b73c54137be0192f1
qinlonghua <long.qin@myirtech.com>
2017-11-10 15:00:02 +0800

    MYIRTECH: 1. increase boot partition and rootfs partition in updatesys.sh script 2. add create matrix-rootfs in post-image.sh script 3. Modify the generation of myir_ricoboard_defconfig is make savedefconfig

--- board/myir/myir_ricoboard/post-image.sh
+++ board/myir/myir_ricoboard/post-image.sh
@@ -17,7 +17,34 @@ cp board/myir/myir_ricoboard/uEnv_ramdisk.txt $BINARIES_DIR/uEnv/uEnv_ramdisk.tx
 cp board/myir/myir_ricoboard/readme.txt $BINARIES_DIR/readme.txt
 mkimage -A arm -O linux -T ramdisk -C none -a 0x88080000 -n "ramdisk" -d $BINARIES_DIR/rootfs.cpio.gz $BINARIES_DIR/ramdisk.gz
 
+cp board/myir/myir_ricoboard/tisdk-rootfs-image-am437x-evm.tar.gz $BINARIES_DIR/matrix-rootfs.tar.gz
 
+mkdir ./tmp
+tar xvf $BINARIES_DIR/matrix-rootfs.tar.gz -C ./tmp
+tar xvf $BINARIES_DIR/rootfs.tar ./lib/modules/4.1.18
+
+cp -a ./tmp/lib/modules/4.1.18-gbbe8cfc/extra/* ./lib/modules/4.1.18/extra/
+cp ./output/build/linux-feature-ricoboard/arch/arm/boot/zImage ./boot
+cp ./output/build/linux-feature-ricoboard/arch/arm/boot/dts/myir_ricoboard.dtb ./boot
+cp ./output/build/linux-feature-ricoboard/arch/arm/boot/dts/myir_ricoboard_hdmi.dtb ./boot
+rm ./lib/modules/4.1.18/extra/omapdrm_pvr.ko
+sed -i '/omapdrm_pvr/d' ./lib/modules/4.1.18/modules.dep
+sed -i '$a\extra/pvrsrvkm.ko:' ./lib/modules/4.1.18/modules.dep
+sed -i '$a\extra/bc_example.ko: extra/pvrsrvkm.ko' ./lib/modules/4.1.18/modules.dep
+sed -i '$a\extra/cryptodev.ko:' ./lib/modules/4.1.18/modules.dep
+
+cp -a ./lib/modules/4.1.18/ ./tmp/lib/modules/
+gzip -d $BINARIES_DIR/matrix-rootfs.tar.gz
+tar rvf $BINARIES_DIR/matrix-rootfs.tar ./lib/modules/4.1.18/
+tar rvf $BINARIES_DIR/matrix-rootfs.tar ./boot/zImage
+tar rvf $BINARIES_DIR/matrix-rootfs.tar ./boot/myir_ricoboard.dtb
+tar rvf $BINARIES_DIR/matrix-rootfs.tar ./boot/myir_ricoboard_hdmi.dtb
+gzip -9 -c $BINARIES_DIR/matrix-rootfs.tar  > $BINARIES_DIR/matrix-rootfs.tar.gz
+rm -rf ./lib/modules/4.1.18
+rm -rf ./tmp/
+rm ./boot/zImage
+rm ./boot/myir_ricoboard.dtb
+rm ./boot/myir_ricoboard_hdmi.dtb
 
 GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
